9a6acd8089b1fdf7bc05ca2ee6bbe7cbc3d69ef2
David Rincon <david.rincon.morales@outlook.com>
2020-01-25 16:25:13 -0600

    Argument Parsing with Yargs Part 1

--- app.js
+++ app.js
@@ -12,14 +12,48 @@
 // BONUS: Use docs to mess around with other styles. Make text bold and inversed
 
 const chalk = require("chalk");
+const yargs = require("yargs");
 const getNotes = require("./notes");
 
-const command = process.argv[2];
+// CUstomize yargs version
+yargs.version("1.1.0");
 
-console.log(process.argv);
+// Create add command
+yargs.command({
+  command: "add",
+  describe: "Add a new note",
+  handler: function() {
+    console.log("Adding a new nnote");
+  }
+});
 
-if (command === "add") {
-  console.log("Adding note");
-} else if (command === "remove") {
-  console.log("Removing Note");
-}
+// Create remove command
+yargs.command({
+  command: "remove",
+  describe: "Remove a note",
+  handler: function() {
+    console.log("Removing the note");
+  }
+});
+
+// Read command
+yargs.command({
+  command: "read",
+  describe: "Read a note",
+  handler: function() {
+    console.log("Reading a note");
+  }
+});
+
+// List command
+yargs.command({
+  command: "list",
+  describe: "List your notes",
+  handler: function() {
+    console.log("Listing all notes");
+  }
+});
+
+//add, remove, read, list notes
+
+console.log(yargs.argv);
