a30908ed1657938922e6f721c30057c4930b6229
David Rincon <david.rincon.morales@outlook.com>
2020-01-27 20:39:06 -0600

    Listing Notes

--- notes.js
+++ notes.js
@@ -5,43 +5,50 @@ const getNotes = () => {
   return "Your notes...";
 };
 
-const addNotes = (title,body) => {
+const addNotes = (title, body) => {
   const notes = loadNotes();
 
   //To check if a title already exists
-  const duplicateNotes = notes.filter(note => { //Zero items if no duplicates
+  const duplicateNotes = notes.filter(note => {
+    //Zero items if no duplicates
     return note.title === title;
   });
 
-  if (duplicateNotes.length === 0){ //Titles not the same
+  if (duplicateNotes.length === 0) {
+    //Titles not the same
     notes.push({
       title: title,
       body: body
     });
-  
+
     saveNotes(notes);
     console.log(chalk.green.inverse("New note added"));
-  }
-  else{
+  } else {
     console.log(chalk.red.inverse("Note title Taken"));
   }
 };
 
 const removeNote = title => {
-  
   const notes = loadNotes();
 
-  const filteredNotes = notes.filter(note => {// Notes to keep
+  const filteredNotes = notes.filter(note => {
+    // Notes to keep
     return note.title !== title;
   });
 
-  if(notes.length > filteredNotes.length){//Note was removed
+  if (notes.length > filteredNotes.length) {
+    //Note was removed
     saveNotes(filteredNotes);
     console.log(chalk.green.inverse("Note removed!"));
+  } else {
+    console.log(chalk.red.inverse("Note not found!"));
   }
-  else{
-    console.log(chalk.red.inverse("Note not found!"))
-  }
+};
+
+const listNodes = () => {
+  const notes = loadNotes();
+  console.log(chalk.blue.inverse("You notes: "));
+  notes.forEach(note => console.log(note.title));
 };
 
 const saveNotes = notes => {
@@ -49,20 +56,24 @@ const saveNotes = notes => {
   fs.writeFileSync("notes.json", dataJSON);
 };
 
-const loadNotes = () => { //Load & Manipulate notes
+const loadNotes = () => {
+  //Load & Manipulate notes
 
-  try { //If notes.json files exists
+  try {
+    //If notes.json files exists
     const dataBuffer = fs.readFileSync("notes.json");
     const dataJSON = dataBuffer.toString();
     return JSON.parse(dataJSON);
-  }
-  catch(errer) { //If file doesnt exist, return an empty array
+  } catch (error) {
+    //If file doesnt exist, return an empty array
     return [];
   }
 };
 
-module.exports = {//Functions to export
-    getNote: getNotes,
-    addNote: addNotes,
-    removeNote: removeNote
-}
+module.exports = {
+  //Functions to export
+  getNote: getNotes,
+  addNote: addNotes,
+  removeNote: removeNote,
+  listNodes: listNodes
+};
